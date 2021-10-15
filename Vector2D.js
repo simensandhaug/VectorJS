ad05e84c17904e1613b88486f0bdc5c29b710357
simensandhaug <simen.sandhaug@gmail.com>
2021-10-15 21:26:37 +0200

    commit

--- Vector2D.js
+++ Vector2D.js
@@ -1,24 +1,25 @@
-class Vector2D {
-    constructor(x, y) {
+var Vector2D = /** @class */ (function () {
+    function Vector2D(x, y) {
+        var _this = this;
+        this.add = function (v) { return new Vector2D(_this.x + v.x, _this.y + v.y); };
+        this.subtract = function (v) { return new Vector2D(_this.x - v.x, _this.y - v.y); };
+        this.subtractValue = function (value) { return new Vector2D(_this.x - value, _this.y - value); };
+        this.multiply = function (value) { return new Vector2D(_this.x * value, _this.y * value); };
+        this.divide = function (value) { return new Vector2D(_this.x / value, _this.y / value); };
+        this.equals = function (v) { return v.x === _this.x && v.y === _this.y; };
+        this.perp = function () { return new Vector2D(-_this.y, _this.x); };
+        this.reverse = function () { return new Vector2D(-_this.x, -_this.y); };
+        this.abs = function () { return new Vector2D(Math.abs(_this.x), Math.abs(_this.y)); };
+        this.mag = function () { return Math.sqrt((_this.x * _this.x) + (_this.y * _this.y)); };
+        this.dot = function (v) { return (_this.x * v.x) + (_this.y * v.y); };
+        this.distance = function (v) { return Math.sqrt((Math.pow((_this.y - v.y), 2)) + (Math.pow((_this.x - v.x), 2))); };
+        this.angle = function () { return Math.atan2(_this.y, _this.x); };
         this.x = x || 0;
         this.y = y || 0;
     }
-    add = (v1, v2) => new Vector2D(v1.x + v2.x, v1.y + v2.y);
-    subtract = (v1, v2) => new Vector2D(v1.x - v2.x, v1.y - v2.y);
-    subtractValue = (v, value) => new Vector2D(v.x - value, v.y - value);
-    multiply = (v, value) => new Vector2D(v.x * value, v.y * value);
-    divide = (v, value) => new Vector2D(v.x / value, v.y / value);
-    equals = (v1, v2) => v1.x === v2.x && v1.y === v2.y;
-    perp = v => new Vector2D(-v.y, v.x);
-    reverse = v => new Vector2D(-v.x, -v.y);
-    abs = v => new Vector2D(Math.abs(v.x), Math.abs(v.y));
-    mag = v => Math.sqrt((v.x * v.x) + (v.y * v.y));
-    dot = (v1, v2) => (v1.x * v2.x) + (v1.y * v2.y);
-    distance = (v1, v2) => Math.sqrt(((v2.y - v1.y)**2) + ((v2.x - v1.x)**2));
-    angle = v => Math.atan2(v.y, v.x);
-
-    setAngle(angle) {
-        this.x = Math.cos(angle) * this.magnitude(this);
-        this.y = Math.sin(angle) * this.magnitude(this);
-    }
-}
+    Vector2D.prototype.setAngle = function (angle) {
+        this.x = Math.cos(angle) * this.mag();
+        this.y = Math.sin(angle) * this.mag();
+    };
+    return Vector2D;
+}());
